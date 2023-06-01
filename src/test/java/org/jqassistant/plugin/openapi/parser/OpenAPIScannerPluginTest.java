a3827c438f8b61bab5276f5d81fed1b1ef8e9171
ansgar (jp06upel) <jp06upel@studserv.uni-leipzig.de>
2023-06-01 11:48:17 +0200

    added tests for paths ( #11 ) also testing relation

--- src/test/java/org/jqassistant/plugin/openapi/parser/OpenAPIScannerPluginTest.java
+++ src/test/java/org/jqassistant/plugin/openapi/parser/OpenAPIScannerPluginTest.java
@@ -6,6 +6,7 @@ import org.jqassistant.plugin.openapi.api.model.*;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
  class OpenAPIScannerPluginTest extends AbstractPluginIT {
@@ -30,6 +31,47 @@ import static org.assertj.core.api.Assertions.*;
          } catch (Exception e){
              fail("Reading contract only containing container data failed", e);
          }
+     }
+
+     @Test
+     @TestStore(type = TestStore.Type.REMOTE)
+     void pathTest(){
+         File file = new File(getClassesDirectory(OpenAPIScannerPluginTest.class), "example-path.yaml");
+         ContractDescriptor contract = getScanner().scan(file, "/example-path.yaml", DefaultScope.NONE);
+
+         store.beginTransaction();
+
+         assertThat(contract).isNotNull();
+
+         List<PathDescriptor> paths = contract.getPaths();
+         assertThat(paths).hasSize(6);      //check if all valid paths are present
+
+         for (PathDescriptor path : paths){
+             assertThat(path).isNotNull();
+             switch (path.getPathUrl()){
+                 case "/path_sample":               // Sanity check
+                 case "/path_sample/sub_path":      // check sub-paths
+                 case "/example_from_sawgger_io":   // real world example from swagger.io
+                     assertThat(path.getOperations()).hasSize(1);
+                     break;
+                 case "/path_with_two_ops":         // path with more than one operation
+                     assertThat(path.getOperations()).hasSize(2);
+                     break;
+                 case "/path_with_nothing":         // empty path entry
+                     fail("contains empty attribute");
+                     break;
+                 case "/path_with_a_/{single}/parameter":   // path with single parameter
+                     assertThat(path.getParameters()).hasSize(1);
+                     break;
+                 case "/path_with_/{t}/w/{o}/parameters":    // path with more than one parameter
+                     assertThat(path.getParameters()).hasSize(2);
+                     break;
+                 default:
+                     fail("not expected path found! (" + path.getPathUrl() + ")");
+             }
+         }
+
+         store.commitTransaction();
      }
 
     @Test
