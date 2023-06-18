c08aed55df011617b5d25c9a3a74e4ba702de82a
realRockstar <you@example.com>
2023-06-18 21:53:02 +0200

    add test class; add example components file

---
+++ src/test/java/org/jqassistant/plugin/openapi/parser/ComponentsTest.java
@@ -0,0 +1,8 @@
+package org.jqassistant.plugin.openapi.parser;
+
+import com.buschmais.jqassistant.core.test.plugin.AbstractPluginIT;
+
+public class ComponentsTest  extends AbstractPluginIT {
+
+
+}
