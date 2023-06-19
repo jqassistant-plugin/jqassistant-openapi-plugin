fb438f1a400f8ca9c3e9d59ecd246bcf16833194
ansgar (jp06upel) <jp06upel@studserv.uni-leipzig.de>
2023-06-19 10:51:11 +0200

    Implement Discriminator descriptor

--- src/main/java/org/jqassistant/plugin/openapi/api/model/jsonschema/SchemaDescriptor.java
+++ src/main/java/org/jqassistant/plugin/openapi/api/model/jsonschema/SchemaDescriptor.java
@@ -3,11 +3,25 @@ package org.jqassistant.plugin.openapi.api.model.jsonschema;
 import com.buschmais.xo.neo4j.api.annotation.Label;
 import com.buschmais.xo.neo4j.api.annotation.Relation;
 
+import java.util.List;
+
 @Label("Schema")
 public interface SchemaDescriptor extends JsonSchemaDescriptor {
     String getName();
     void setName(String name);
 
     @Relation("HAS_PROPERTY")
-    PropertyDescriptor getProperty();
+    List<PropertyDescriptor> getProperties();
+
+    @Relation("ALL_OF")
+    DiscriminatorDescriptor getDiscriminatorAllOf();
+    void setDiscriminatorAllOf(DiscriminatorDescriptor discriminator);
+
+    @Relation("ONE_OF")
+    DiscriminatorDescriptor getDiscriminatorOneOf();
+    void setDiscriminatorOneOf(DiscriminatorDescriptor discriminator);
+
+    @Relation("ANY_OF")
+    DiscriminatorDescriptor getDiscriminatorAnyOf();
+    void setDiscriminatorAnyOf(DiscriminatorDescriptor discriminator);
 }
