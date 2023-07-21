660b1e18c0c88ae65908a9b31a2388a2b2defb88
CodeOfRob <fnacercoding@gmail.com>
2023-07-21 12:10:48 +0200

    switched parsing functions to use new PathParser

--- src/main/java/org/jqassistant/plugin/openapi/impl/ContractParser.java
+++ src/main/java/org/jqassistant/plugin/openapi/impl/ContractParser.java
@@ -36,7 +36,7 @@ public class ContractParser {
 
         LOG.info("Reading OpenAPI Paths");
         if(contract.getPaths() != null && !contract.getPaths().isEmpty())
-            contractDescriptor.getPaths().addAll(Parsers.parsePaths(contract.getPaths(), store));
+            contractDescriptor.getPaths().addAll(PathParser.parseAll(contract.getPaths(), store));
     }
 
     /**
