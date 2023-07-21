1d716be24fbdbc86c3273a886634d232728b709f
Robin Kupfer <fnacercoding@gmail.com>
2023-07-21 18:45:34 +0200

    removed code smell

--- src/test/java/org/jqassistant/plugin/openapi/parser/ComponentsCountTest.java
+++ src/test/java/org/jqassistant/plugin/openapi/parser/ComponentsCountTest.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class ComponentsCountTest extends AbstractPluginIT {
+class ComponentsCountTest extends AbstractPluginIT {
 
     ContractDescriptor contract;
 
