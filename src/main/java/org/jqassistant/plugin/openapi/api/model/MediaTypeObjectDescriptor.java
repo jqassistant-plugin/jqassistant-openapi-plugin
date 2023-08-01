c0e4ad7f003f4afae49a85b6c5d45bd0af724287
Robin Kupfer <fnacercoding@gmail.com>
2023-08-01 11:22:16 +0200

    introduced EncodingDescriptor, enhanced MediaTypeObjectDescriptor, enhanced parsing

--- src/main/java/org/jqassistant/plugin/openapi/api/model/MediaTypeObjectDescriptor.java
+++ src/main/java/org/jqassistant/plugin/openapi/api/model/MediaTypeObjectDescriptor.java
@@ -1,9 +1,25 @@
 package org.jqassistant.plugin.openapi.api.model;
 
 import com.buschmais.xo.neo4j.api.annotation.Label;
+import com.buschmais.xo.neo4j.api.annotation.Relation;
+import org.jqassistant.plugin.openapi.api.model.jsonschema.JsonSchemaDescriptor;
+import java.util.List;
 
 @Label("MediaTypeObject")
 public interface MediaTypeObjectDescriptor extends OpenApiDescriptor{
     String getMediaType();
     void setMediaType(String mediaType);
+
+    @Relation("DEFINED_BY")
+    JsonSchemaDescriptor getSchema();
+    void setSchema(JsonSchemaDescriptor schema);
+
+    Object getExample();
+    void setExample(Object example);
+
+    @Relation("PROVIDES")
+    List<ExampleDescriptor> getExamples();
+
+    @Relation("DEFINED_BY")
+    List<EncodingDescriptor> getEncodings();
 }
