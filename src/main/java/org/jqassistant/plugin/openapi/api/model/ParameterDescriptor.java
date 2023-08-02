663bfb3aec3d8bfa6831eb724a0f122b8ec9392c
Robin Kupfer <fnacercoding@gmail.com>
2023-08-02 14:57:24 +0200

    extended ParameterDescriptor to cover all fixed fields

--- src/main/java/org/jqassistant/plugin/openapi/api/model/ParameterDescriptor.java
+++ src/main/java/org/jqassistant/plugin/openapi/api/model/ParameterDescriptor.java
@@ -2,6 +2,11 @@ package org.jqassistant.plugin.openapi.api.model;
 
 
 import com.buschmais.xo.neo4j.api.annotation.Label;
+import com.buschmais.xo.neo4j.api.annotation.Relation;
+import io.swagger.v3.oas.models.parameters.Parameter;
+import org.jqassistant.plugin.openapi.api.model.jsonschema.SchemaDescriptor;
+
+import java.util.List;
 
 @Label("Parameter")
 public interface ParameterDescriptor extends OpenApiDescriptor{
@@ -11,14 +16,41 @@ public interface ParameterDescriptor extends OpenApiDescriptor{
 
     String getName();
     void setName(String name);
+
     ParameterLocation getLocation();
     void setLocation(ParameterLocation location);
+
     String getDescription();
     void setDescription(String description);
+
     Boolean getIsRequired();
     void setIsRequired(Boolean isRequired);
+
     Boolean getIsDeprecated();
     void setIsDeprecated(Boolean isDeprecated);
+
     Boolean getAllowsEmptyValue();
     void setAllowsEmptyValue(Boolean allowsEmptyValue);
+
+    Parameter.StyleEnum getStyle();
+    void setStyle(Parameter.StyleEnum style);
+
+    boolean getExplode();
+    void setExplode(boolean explode);
+
+    boolean getAllowsReserved();
+    void setAllowsReserved(boolean allowsReserved);
+
+    @Relation("PROVIDES")
+    SchemaDescriptor getSchema();
+    void setSchema(SchemaDescriptor schema);
+
+    Object getExample();
+    void setExample(Object example);
+
+    @Relation("PROVIDES")
+    List<ExampleDescriptor> getExamples();
+
+    @Relation("CONTAINS")
+    List<MediaTypeObjectDescriptor> getContent();
 }
