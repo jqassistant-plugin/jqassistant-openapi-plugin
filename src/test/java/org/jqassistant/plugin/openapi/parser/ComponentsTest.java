0895f8792cdaf7766b815b864cc34789845e1e05
Robin Kupfer <fnacercoding@gmail.com>
2023-08-02 16:08:07 +0200

    enhanced parameter tests

--- src/test/java/org/jqassistant/plugin/openapi/parser/ComponentsTest.java
+++ src/test/java/org/jqassistant/plugin/openapi/parser/ComponentsTest.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import com.buschmais.jqassistant.core.scanner.api.DefaultScope;
 import com.buschmais.jqassistant.core.test.plugin.AbstractPluginIT;
+import io.swagger.v3.oas.models.parameters.Parameter;
 import org.jqassistant.plugin.openapi.api.model.*;
 import org.jqassistant.plugin.openapi.api.model.jsonschema.SchemaDescriptor;
 import org.junit.jupiter.api.AfterEach;
@@ -75,14 +76,84 @@ class ComponentsTest extends AbstractPluginIT {
     }
 
     @Test
-    void testParameters() {
+    void testParameterRelation(){
         List<ParameterDescriptor> parameters = contract.getComponents().getParameters();
-        assertThat(parameters).hasSize(1);
+        assertThat(parameters).hasSize(4);
+    }
+
+    @Test
+    void testRichParameters() {
+        ParameterDescriptor paramAllFieldsExample = getParamByName("param_all_fields_example");
+        assertThat(paramAllFieldsExample.getName()).isEqualTo("param_all_fields_example");
+        assertThat(paramAllFieldsExample.getLocation()).isEqualTo(ParameterDescriptor.ParameterLocation.HEADER);
+        assertThat(paramAllFieldsExample.getDescription()).isEqualTo("token to be passed as a header");
+        assertThat(paramAllFieldsExample.getIsRequired()).isTrue();
+        assertThat(paramAllFieldsExample.getIsDeprecated()).isTrue();
+        assertThat(paramAllFieldsExample.getAllowsEmptyValue()).isNull(); // null when location is HEADER
+        assertThat(paramAllFieldsExample.getStyle()).isEqualTo(Parameter.StyleEnum.SIMPLE);
+        assertThat(paramAllFieldsExample.getExplode()).isTrue();
+        assertThat(paramAllFieldsExample.getAllowsReserved()).isTrue();
+        assertThat(paramAllFieldsExample.getSchema()).isNotNull();
+        assertThat(paramAllFieldsExample.getExample()).isNotNull();
+        assertThat(paramAllFieldsExample.getExamples()).isEmpty();
+
+        ParameterDescriptor paramAllFieldsExamples = getParamByName("param_all_fields_examples");
+        assertThat(paramAllFieldsExamples.getName()).isEqualTo("param_all_fields_examples");
+        assertThat(paramAllFieldsExamples.getLocation()).isEqualTo(ParameterDescriptor.ParameterLocation.QUERY);
+        assertThat(paramAllFieldsExamples.getDescription()).isEqualTo("token to be passed as a header");
+        assertThat(paramAllFieldsExamples.getIsRequired()).isFalse();
+        assertThat(paramAllFieldsExamples.getIsDeprecated()).isFalse();
+        assertThat(paramAllFieldsExamples.getAllowsEmptyValue()).isFalse(); // null when location is HEADER
+        assertThat(paramAllFieldsExamples.getStyle()).isEqualTo(Parameter.StyleEnum.SIMPLE);
+        assertThat(paramAllFieldsExamples.getExplode()).isFalse();
+        assertThat(paramAllFieldsExamples.getAllowsReserved()).isFalse();
+        assertThat(paramAllFieldsExamples.getSchema()).isNotNull();
+        assertThat(paramAllFieldsExamples.getExample()).isNull();
+        assertThat(paramAllFieldsExamples.getExamples()).hasSize(2);
+    }
+
+    @Test
+    void testEmptyParameters(){
+        ParameterDescriptor paramEmptyFieldsExamples = getParamByName("param_empty_fields");
+        assertThat(paramEmptyFieldsExamples.getName()).isEqualTo("param_empty_fields");
+        assertThat(paramEmptyFieldsExamples.getLocation()).isEqualTo(ParameterDescriptor.ParameterLocation.HEADER);
+        assertThat(paramEmptyFieldsExamples.getDescription()).isNull();
+        assertThat(paramEmptyFieldsExamples.getIsRequired()).isFalse();
+        assertThat(paramEmptyFieldsExamples.getIsDeprecated()).isNull();
+        assertThat(paramEmptyFieldsExamples.getAllowsEmptyValue()).isNull(); // null when location is HEADER
+        assertThat(paramEmptyFieldsExamples.getStyle()).isEqualTo(Parameter.StyleEnum.SIMPLE);
+        assertThat(paramEmptyFieldsExamples.getExplode()).isFalse();
+        assertThat(paramEmptyFieldsExamples.getAllowsReserved()).isFalse();
+        assertThat(paramEmptyFieldsExamples.getSchema()).isNull();
+        assertThat(paramEmptyFieldsExamples.getExample()).isNull();
+        assertThat(paramEmptyFieldsExamples.getExamples()).isEmpty();
+
+        ParameterDescriptor paramNoFieldsExamples = getParamByName("param_no_fields");
+        assertThat(paramNoFieldsExamples.getName()).isEqualTo("param_no_fields");
+        assertThat(paramNoFieldsExamples.getLocation()).isEqualTo(ParameterDescriptor.ParameterLocation.HEADER);
+        assertThat(paramNoFieldsExamples.getDescription()).isNull();
+        assertThat(paramNoFieldsExamples.getIsRequired()).isFalse();
+        assertThat(paramNoFieldsExamples.getIsDeprecated()).isNull();
+        assertThat(paramNoFieldsExamples.getAllowsEmptyValue()).isNull(); // null when location is HEADER
+        assertThat(paramNoFieldsExamples.getStyle()).isEqualTo(Parameter.StyleEnum.SIMPLE);
+        assertThat(paramNoFieldsExamples.getExplode()).isFalse();
+        assertThat(paramNoFieldsExamples.getAllowsReserved()).isFalse();
+        assertThat(paramNoFieldsExamples.getSchema()).isNull();
+        assertThat(paramNoFieldsExamples.getExample()).isNull();
+        assertThat(paramNoFieldsExamples.getExamples()).isEmpty();
     }
 
     @Test
     void testSchemas() {
         List<SchemaDescriptor> schemas = contract.getComponents().getSchemas();
         assertThat(schemas).hasSize(1);
+    }
+
+    private ParameterDescriptor getParamByName(String name){
+        List<ParameterDescriptor> params = contract.getComponents().getParameters();
+        for(ParameterDescriptor param: params)
+            if(param.getName().equals(name))
+                return param;
+        return null;
     }
 }
