18891ea7ee11e51a732fb5d633ebb33e1c5c8f41
Robin Kupfer <fnacercoding@gmail.com>
2023-08-02 12:14:25 +0200

    added tests for properties and relations

--- src/test/java/org/jqassistant/plugin/openapi/parser/OpenAPIScannerPluginTest.java
+++ src/test/java/org/jqassistant/plugin/openapi/parser/OpenAPIScannerPluginTest.java
@@ -9,36 +9,35 @@ import java.io.File;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
- class OpenAPIScannerPluginTest extends AbstractPluginIT {
+class OpenAPIScannerPluginTest extends AbstractPluginIT {
 
-     ContractDescriptor contract;
+    ContractDescriptor contract;
 
-     @Test
-     void scanNullContract(){
-         File file = new File(getClassesDirectory(OpenAPIScannerPluginTest.class), "example-nulltest.yaml");
-         try {
-             contract = getScanner().scan(file, "example-nulltest.yaml", DefaultScope.NONE);
-             assertThat(contract).isNotNull();
-         } catch (Exception e){
+    @Test
+    void scanNullContract(){
+        File file = new File(getClassesDirectory(OpenAPIScannerPluginTest.class), "example-nulltest.yaml");
+        try {
+            contract = getScanner().scan(file, "example-nulltest.yaml", DefaultScope.NONE);
+            assertThat(contract).isNotNull();
+        } catch (Exception e){
             fail("Reading contract not containing any data failed", e);
-         }
-     }
+        }
+    }
 
-     @Test
-     void scanEmptyContract(){  
-         File file = new File(getClassesDirectory(OpenAPIScannerPluginTest.class), "example-emptytest.yaml");
-         try {
-             contract = getScanner().scan(file, "example-emptytest.yaml", DefaultScope.NONE);
-             assertThat(contract).isNotNull();
-         } catch (Exception e){
-             fail("Reading contract only containing container data failed", e);
-         }
-     }
+    @Test
+    void scanEmptyContract(){
+        File file = new File(getClassesDirectory(OpenAPIScannerPluginTest.class), "example-emptytest.yaml");
+        try {
+            contract = getScanner().scan(file, "example-emptytest.yaml", DefaultScope.NONE);
+            assertThat(contract).isNotNull();
+        } catch (Exception e){
+            fail("Reading contract only containing container data failed", e);
+        }
+    }
 
 
     @Test
     void scanMetaData(){
-
         File testFile = new File(getClassesDirectory(OpenAPIScannerPluginTest.class), "example-metadata.yaml");
         contract = getScanner().scan(testFile, "/example-metadata.yaml", DefaultScope.NONE);
 
@@ -54,7 +53,7 @@ import static org.assertj.core.api.Assertions.*;
         ServerDescriptor server = contract.getServers().get(0);
         assertThat(server.getUrl()).isEqualTo("/rest/v1/users");
 
-        assertThat(contract.getTags()).hasSize(4);
+        assertThat(contract.getTags()).hasSize(7);
         TagDescriptor tag = getTagByName("issues");
         assertThat(tag.getTag()).isEqualTo("issues");
         assertThat(tag.getDescription()).isEqualTo("Issues API");
@@ -66,54 +65,79 @@ import static org.assertj.core.api.Assertions.*;
         store.commitTransaction();
     }
 
-     @Test
-     void scanWholeContract(){
-         File file = new File(getClassesDirectory(OpenAPIScannerPluginTest.class), "example.yaml");
-         try {
-             ContractDescriptor contract = getScanner().scan(file, "example.yaml", DefaultScope.NONE);
-             assertThat(contract).isNotNull();
-         } catch (Exception e){
-             fail("Reading whole example contract failed", e);
-         }
-     }
+    @Test
+    void scanTags(){
+        File testFile = new File(getClassesDirectory(OpenAPIScannerPluginTest.class), "example-metadata.yaml");
+        contract = getScanner().scan(testFile, "/example-metadata.yaml", DefaultScope.NONE);
 
-     @Test
-     void testExternalDocs(){
+        store.beginTransaction();
+        assertThat(contract).isNotNull();
+        assertThat(contract.getTags()).hasSize(7);
 
-         File file = new File(getClassesDirectory(OpenAPIScannerPluginTest.class), "example-metadata.yaml");
-         try {
-             contract = getScanner().scan(file, "example-metadata.yaml", DefaultScope.NONE);
-             assertThat(contract).isNotNull();
-         } catch (Exception e){
-             fail("Reading contract only containing container data failed", e);
-         }
+        TagDescriptor tagAllFields = getTagByName("tag_with_all_fields");
+        assertThat(tagAllFields.getTag()).isEqualTo("tag_with_all_fields");
+        assertThat(tagAllFields.getDescription()).isEqualTo("This is a tag with all fields set");
+        assertThat(tagAllFields.getExternalDocs()).isNotNull();
 
-         store.beginTransaction();
-         ExternalDocsDescriptor externalDocsWithProperties = getTagByName("issues").getExternalDocs();
-         assertThat(externalDocsWithProperties.getDescription()).isEqualTo("issues - external docs description");
-         assertThat(externalDocsWithProperties.getUrl()).isEqualTo("https://www.1.example.com");
+        TagDescriptor tagEmptyFields = getTagByName("tag_with_empty_fields");
+        assertThat(tagEmptyFields.getTag()).isEqualTo("tag_with_empty_fields");
+        assertThat(tagEmptyFields.getDescription()).isNull();
+        assertThat(tagEmptyFields.getExternalDocs()).isNull();
 
-         ExternalDocsDescriptor externalDocsWithoutDescription = getTagByName("externalDocs_without_description").getExternalDocs();
-         assertThat(externalDocsWithoutDescription.getDescription()).isNull();
-         assertThat(externalDocsWithoutDescription.getUrl()).isEqualTo("https://www.2.example.com");
+        TagDescriptor tagNoFields = getTagByName("tag_with_no_fields");
+        assertThat(tagNoFields.getTag()).isEqualTo("tag_with_no_fields");
+        assertThat(tagNoFields.getDescription()).isNull();
+        assertThat(tagNoFields.getExternalDocs()).isNull();
 
-         ExternalDocsDescriptor externalDocsWithoutUrl = getTagByName("externalDocs_without_url").getExternalDocs();
-         assertThat(externalDocsWithoutUrl.getDescription()).isEqualTo("external docs without url");
-         assertThat(externalDocsWithoutUrl.getUrl()).isNull();
+        store.commitTransaction();
+    }
 
-         ExternalDocsDescriptor externalDocsWithEmptyProps = getTagByName("externalDocs_with_empty_props").getExternalDocs();
-         assertThat(externalDocsWithEmptyProps.getDescription()).isNull();
-         assertThat(externalDocsWithEmptyProps.getUrl()).isNull();
+    @Test
+    void scanWholeContract(){
+        File file = new File(getClassesDirectory(OpenAPIScannerPluginTest.class), "example.yaml");
+        try {
+            ContractDescriptor contract = getScanner().scan(file, "example.yaml", DefaultScope.NONE);
+            assertThat(contract).isNotNull();
+        } catch (Exception e){
+            fail("Reading whole example contract failed", e);
+        }
+    }
 
-         store.commitTransaction();
+    @Test
+    void testExternalDocs(){
+        File file = new File(getClassesDirectory(OpenAPIScannerPluginTest.class), "example-metadata.yaml");
+        try {
+            contract = getScanner().scan(file, "example-metadata.yaml", DefaultScope.NONE);
+            assertThat(contract).isNotNull();
+        } catch (Exception e){
+            fail("Reading contract only containing container data failed", e);
         }
 
-     private TagDescriptor getTagByName(String name){
-         List<TagDescriptor> tags = contract.getTags();
-         for(TagDescriptor tag: tags)
-             if(tag.getTag().equals(name))
-                 return tag;
-         return null;
-     }
+        store.beginTransaction();
+        ExternalDocsDescriptor externalDocsWithProperties = getTagByName("issues").getExternalDocs();
+        assertThat(externalDocsWithProperties.getDescription()).isEqualTo("issues - external docs description");
+        assertThat(externalDocsWithProperties.getUrl()).isEqualTo("https://www.1.example.com");
 
+        ExternalDocsDescriptor externalDocsWithoutDescription = getTagByName("externalDocs_without_description").getExternalDocs();
+        assertThat(externalDocsWithoutDescription.getDescription()).isNull();
+        assertThat(externalDocsWithoutDescription.getUrl()).isEqualTo("https://www.2.example.com");
+
+        ExternalDocsDescriptor externalDocsWithoutUrl = getTagByName("externalDocs_without_url").getExternalDocs();
+        assertThat(externalDocsWithoutUrl.getDescription()).isEqualTo("external docs without url");
+        assertThat(externalDocsWithoutUrl.getUrl()).isNull();
+
+        ExternalDocsDescriptor externalDocsWithEmptyProps = getTagByName("externalDocs_with_empty_props").getExternalDocs();
+        assertThat(externalDocsWithEmptyProps.getDescription()).isNull();
+        assertThat(externalDocsWithEmptyProps.getUrl()).isNull();
+
+        store.commitTransaction();
+    }
+
+    private TagDescriptor getTagByName(String name){
+        List<TagDescriptor> tags = contract.getTags();
+        for(TagDescriptor tag: tags)
+            if(tag.getTag().equals(name))
+                return tag;
+        return null;
+    }
 }
