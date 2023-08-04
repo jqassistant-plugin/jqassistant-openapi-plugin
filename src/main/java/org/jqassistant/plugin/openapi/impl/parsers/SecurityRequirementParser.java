a0f87f5b20221d67a7a1f23930a12c52cc0191b7
Robin Kupfer <fnacercoding@gmail.com>
2023-08-04 16:01:33 +0200

    added template of SecurityRequirementParser

--- src/main/java/org/jqassistant/plugin/openapi/impl/parsers/SecurityRequirementParser.java
+++ src/main/java/org/jqassistant/plugin/openapi/impl/parsers/SecurityRequirementParser.java
@@ -1,10 +1,11 @@
 package org.jqassistant.plugin.openapi.impl.parsers;
 
+import com.buschmais.jqassistant.core.store.api.Store;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import org.jqassistant.plugin.openapi.api.model.SecurityRequirementDescriptor;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class SecurityRequirementParser {
 
@@ -12,7 +13,11 @@ public class SecurityRequirementParser {
         throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
     }
 
-    public static List<SecurityRequirementDescriptor> parseAll(List<SecurityRequirement> securityRequirements){
-        return new ArrayList<>();
+    public static List<SecurityRequirementDescriptor> parseAll(List<SecurityRequirement> securityRequirements, Store store){
+        return securityRequirements.stream().map(securityRequirement -> parseOne(securityRequirement, store)).collect(Collectors.toList());
+    }
+
+    public static SecurityRequirementDescriptor parseOne(SecurityRequirement securityRequirement, Store store){
+        return store.create(SecurityRequirementDescriptor.class);
     }
 }
