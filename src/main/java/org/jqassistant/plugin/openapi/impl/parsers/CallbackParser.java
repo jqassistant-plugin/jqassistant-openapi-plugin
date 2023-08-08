07c2a845e213a4814cabb9ae1a9523fb674b4441
Robin Kupfer <fnacercoding@gmail.com>
2023-08-08 14:19:12 +0200

    extended CallbackDescriptor and according parser

--- src/main/java/org/jqassistant/plugin/openapi/impl/parsers/CallbackParser.java
+++ src/main/java/org/jqassistant/plugin/openapi/impl/parsers/CallbackParser.java
@@ -1,11 +1,13 @@
 package org.jqassistant.plugin.openapi.impl.parsers;
 
 import com.buschmais.jqassistant.core.store.api.Store;
+import io.swagger.v3.oas.models.PathItem;
 import io.swagger.v3.oas.models.callbacks.Callback;
 import org.jqassistant.plugin.openapi.api.model.CallbackDescriptor;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class CallbackParser {
@@ -15,14 +17,18 @@ public class CallbackParser {
     }
 
     public static List<CallbackDescriptor> parseAll(Map<String, Callback> callbacksMap, Store store){
-        return callbacksMap.values().stream().map(callback -> parseOne(callback, store)).collect(Collectors.toList());
+        return callbacksMap.entrySet().stream().map(callbackEntry -> parseOne(callbackEntry.getKey(), callbackEntry.getValue(), store)).collect(Collectors.toList());
     }
 
-    public static CallbackDescriptor parseOne(Callback callback, Store store){
+    public static CallbackDescriptor parseOne(String name, Callback callback, Store store){
         CallbackDescriptor callbackDescriptor = store.create(CallbackDescriptor.class);
+
+        callbackDescriptor.setName(name);
 
         if(callback.get$ref() != null)
             callbackDescriptor.setRef(callback.get$ref());
+
+        callbackDescriptor.getPathItems().addAll(PathsParser.parsePathItems(callback, store));
 
         return callbackDescriptor;
     }
