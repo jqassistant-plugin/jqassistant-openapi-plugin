6962d69650e4e320e27bd801cd366dbbbebfdd62
Robin Kupfer <fnacercoding@gmail.com>
2023-08-09 18:11:08 +0200

    cleaned RequestBodyParsers parseOne function

--- src/main/java/org/jqassistant/plugin/openapi/impl/parsers/RequestBodyParser.java
+++ src/main/java/org/jqassistant/plugin/openapi/impl/parsers/RequestBodyParser.java
@@ -19,15 +19,12 @@ public class RequestBodyParser {
     public static RequestBodyDescriptor parseOne(RequestBody requestBody, Store store){
         RequestBodyDescriptor requestBodyDescriptor = store.create(RequestBodyDescriptor.class);
 
-        // read properties
-        if(requestBody.getDescription() != null && !requestBody.getDescription().isEmpty())
+        if(requestBody.getDescription() != null)
             requestBodyDescriptor.setDescription(requestBody.getDescription());
+        if(requestBody.getContent() != null)
+            requestBodyDescriptor.getMediaTypeObjects().addAll(MediaTypeParser.parseAll(requestBody.getContent(), store));
         if(requestBody.getRequired() != null)
             requestBodyDescriptor.setIsRequired(requestBody.getRequired());
-
-        // read content
-        if(requestBody.getContent() != null && !requestBody.getContent().isEmpty())
-            requestBodyDescriptor.getMediaTypeObjects().addAll(MediaTypeParser.parseAll(requestBody.getContent(), store));
 
         return requestBodyDescriptor;
     }
