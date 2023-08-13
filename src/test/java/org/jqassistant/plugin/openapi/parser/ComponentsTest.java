9af22d5a639e4daf4eec495fdf54071c6b553993
Robin Kupfer <fnacercoding@gmail.com>
2023-08-13 17:52:39 +0200

    fixed failing test

--- src/test/java/org/jqassistant/plugin/openapi/parser/ComponentsTest.java
+++ src/test/java/org/jqassistant/plugin/openapi/parser/ComponentsTest.java
@@ -120,7 +120,7 @@ class ComponentsTest extends AbstractPluginIT {
         List<ResponseDescriptor> responses = contract.getComponents().getResponses();
         assertThat(responses).hasSize(5);
 
-        ResponseDescriptor resDefault = getResponse("default");
+        ResponseDescriptor resDefault = getResponseByStatusCodeOrDefault("default");
         assertThat(resDefault.getIsDefault()).isTrue();
         assertThat(resDefault.getStatusCode()).isNull();
         assertThat(resDefault.getDescription()).isEqualTo("a default response with all fields");
@@ -128,7 +128,7 @@ class ComponentsTest extends AbstractPluginIT {
         assertThat(resDefault.getMediaTypeObjects()).hasSize(1);
         assertThat(resDefault.getLinks()).hasSize(1);
 
-        ResponseDescriptor resEmpty = getResponse("433");
+        ResponseDescriptor resEmpty = getResponseByStatusCodeOrDefault("433");
         assertThat(resEmpty.getIsDefault()).isFalse();
         assertThat(resEmpty.getStatusCode()).isEqualTo("433");
         assertThat(resEmpty.getDescription()).isNull();
@@ -136,7 +136,7 @@ class ComponentsTest extends AbstractPluginIT {
         assertThat(resEmpty.getMediaTypeObjects()).isEmpty();
         assertThat(resEmpty.getLinks()).isEmpty();
 
-        ResponseDescriptor resNoFields = getResponse("434");
+        ResponseDescriptor resNoFields = getResponseByStatusCodeOrDefault("434");
         assertThat(resNoFields.getIsDefault()).isFalse();
         assertThat(resNoFields.getStatusCode()).isEqualTo("434");
         assertThat(resNoFields.getDescription()).isEqualTo("no fields");
@@ -147,7 +147,7 @@ class ComponentsTest extends AbstractPluginIT {
 
     @Test
     void testMediaTypeObjects(){
-        ResponseDescriptor response404 = getResponseByName("418");
+        ResponseDescriptor response404 = getResponseByStatusCodeOrDefault("418");
         assertThat(response404.getMediaTypeObjects()).hasSize(3);
 
         MediaTypeObjectDescriptor mtoExample = getMediaTypeObjectByName("multipart/form-data_example");
@@ -293,7 +293,7 @@ class ComponentsTest extends AbstractPluginIT {
         return null;
     }
 
-    private ResponseDescriptor getResponse(String statusCodeOrDefault){
+    private ResponseDescriptor getResponseByStatusCodeOrDefault(String statusCodeOrDefault){
         List<ResponseDescriptor> responses = contract.getComponents().getResponses();
         for(ResponseDescriptor response: responses) {
             if (response.getIsDefault() && statusCodeOrDefault.equals("default"))
@@ -301,14 +301,6 @@ class ComponentsTest extends AbstractPluginIT {
             if (!statusCodeOrDefault.equals("default") && statusCodeOrDefault.equals(response.getStatusCode()))
                 return response;
         }
-        return null;
-    }
-
-    private ResponseDescriptor getResponseByName(String name){
-        List<ResponseDescriptor> responses = contract.getComponents().getResponses();
-        for(ResponseDescriptor response: responses)
-            if(response.getStatusCode().equals(name))
-                return response;
         return null;
     }
 
